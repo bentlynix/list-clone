e8d505b38537f21fa77baf07efa754fd97ad9dd5
bently <68941207+bentlynixon@users.noreply.github.com>
2020-09-05 08:05:50 +0530

    Initial commit

---
+++ public/javascripts/views/cards.js
@@ -0,0 +1,15 @@
+var CardsView = Backbone.View.extend({
+  template: App.templates.cards,
+  render: function () {
+    $("main")
+      .find("ul[data-id=" + this.id + "]")
+      .html(
+        this.template({
+          card: JSON.parse(JSON.stringify(this.collection)),
+        })
+      );
+  },
+  initialize: function () {
+    this.render();
+  },
+});
